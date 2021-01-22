0cd412c9b5a03aaafeba3146f8be69542e50c318
Casey Haskins <cahaskins@bsu.edu>
2021-01-22 18:45:39 -0500

    Bonus Attempt

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -1,18 +1,33 @@
+import java.util.*;
+
 public class InsuranceCalculator {
 
     private InsuranceStrategy strategy;
+    private int[] insuranceBrackets = {10000, 35600, 76500, Integer.MAX_VALUE};
+    private HashMap<Integer, InsuranceStrategy> mapping = new HashMap();
+
+    public InsuranceCalculator() {
+        mapping.put(0, new InsuranceStrategyLow());
+        mapping.put(1, new InsuranceStrategyMedium());
+        mapping.put(2, new InsuranceStrategyHigh());
+        mapping.put(3, new InsuranceStrategyVeryHigh());
+    }
 
     public double calculateInsurance(double income) {
-        if (income <= 10000) {
-            strategy = new InsuranceStrategyLow();
-        } else if (income <= 30000) {
-            strategy = new InsuranceStrategyMedium();
-        } else if (income <= 60000) {
-            strategy = new InsuranceStrategyHigh();
-        } else {
-            strategy = new InsuranceStrategyVeryHigh();
-        }
+        strategy = getInsuranceStrategy(income);
         return strategy.calculate(income);
+    }
+
+    private InsuranceStrategy getInsuranceStrategy(double income) {
+        Integer index = 0;
+        for (int i = 0; i<insuranceBrackets.length; i++) {
+            if (income <= insuranceBrackets[i]) {
+                index = i;
+                break;
+            }
+        }
+
+        return mapping.get(index);
     }
 
 }
